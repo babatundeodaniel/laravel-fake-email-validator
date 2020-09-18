b9f77f769cd13379fc4b6cbf85e1868021dc013b
Babatunde Daniel <daniel.babatunde@upperlink.ng>
2020-09-18 15:15:46 +0100

    Playing with reading file

--- src/GetripayVerifyFakeEmails.php
+++ src/GetripayVerifyFakeEmails.php
@@ -4,15 +4,29 @@ namespace Getripay\GetripayVerifyFakeEmails;
 
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\LazyCollection;
 
 class GetripayVerifyFakeEmails
 {
     public $path_to_file = 'vendor/wesbos/burner-email-providers/emails.txt';
     // Build your next great package.
     public function validate ($attribute, $value, $parameters, $validator) {
-        $collection = new Collection();
-        $contents = Storage::get($this->path_to_file);
-        dump($contents);
+        //$collection = new Collection();
+        $collection  = LazyCollection::make(function (){
+            $handle = fopen(base_path().$this->path_to_file, "r+");
+            while (($line = fgets($handle)) !== false) {
+                yield $line;
+            }
+        });
+        logger(print_r($collection->take(100)->all(), 1));
         return $value == 'foo';
+    }
+
+    public function readFileToCache(){
+        $fp = fopen(base_path().$this->path_to_file, "r+");
+        while ($line = stream_get_line($fp, 1024 * 1024, "\n")) {
+            echo $line;
+        }
+        fclose($fp);
     }
 }
